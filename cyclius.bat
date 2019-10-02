8bcd34ffb6121459a8a88619df5be9751437328c
hutcheson8 <hutcheson8@gmail.com>
2019-10-02 16:47:13 -0500

    Added Code for Cyclius

---
+++ cyclius.bat
@@ -0,0 +1,3 @@
+@echo off
+node cyclius.js
+pause
---
+++ cyclius.js
@@ -0,0 +1,3 @@
+console.log("Diamond Bonus is: \t"+(15*Math.sin((Date.now()/1000/(60*60*3))*Math.PI*2))+"%");
+console.log("Ruby Bonus is: \t\t"+(15*Math.sin((Date.now()/1000/(60*60*12))*Math.PI*2))+"%");
+console.log("Jade Bonus is: \t\t"+(15*Math.sin((Date.now()/1000/(60*60*24))*Math.PI*2))+"%");
